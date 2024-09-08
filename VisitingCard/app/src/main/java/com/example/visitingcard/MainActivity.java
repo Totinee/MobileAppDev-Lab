a7b4969d607389c5490facf912bb75cd582f6fff
Hasneen Tamanna Totinee <jucse29.362@gmail.com>
2024-09-09 02:48:51 +0600

    Uploaded android projects

---
+++ VisitingCard/app/src/main/java/com/example/visitingcard/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.visitingcard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // click contact num, direct to dial pad
+        TextView contactNumber = findViewById(R.id.contact_number);
+        contactNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = "+8801521571755";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                startActivity(intent);
+            }
+        });
+
+        // click email, direct to gmail app
+        TextView email = findViewById(R.id.email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:totinee.stu2019@juniv.edu"));
+                startActivity(intent);
+            }
+        });
+
+        // click and direct to linked in profile
+        TextView linkedin = findViewById(R.id.linkedin);
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String linkedInUrl = "https://www.linkedin.com/in/hasneen-tamanna-totinee-096a801b2/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedInUrl));
+                startActivity(intent);
+            }
+        });
+
+        // click and direct to whatsapp chat
+        TextView whatsapp = findViewById(R.id.whatsapp);
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = "+8801521571755";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://api.whatsapp.com/send?phone=" + phone));
+                startActivity(intent);
+            }
+        });
+    }
+}
